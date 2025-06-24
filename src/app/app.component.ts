81d0835f7eee276800cde8df8ac8d16e97579df3
Bart Hermans <bart.hermans@hva.nl>
2025-06-24 15:19:14 +0200

    Created the whoami componet with styling and some easy animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./components/hero/hero.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { WhoAmIComponent } from "./components/who-am-i/who-am-i.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeroComponent, NavbarComponent]
+  imports: [HeroComponent, NavbarComponent, WhoAmIComponent]
 })
 export class AppComponent {
 }
---
+++ src/app/components/who-am-i/who-am-i.component.ts
@@ -0,0 +1,79 @@
+import { CommonModule } from '@angular/common';
+import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import * as Prism from 'prismjs'
+import 'prismjs/components/prism-csharp.min.js';
+
+@Component({
+  selector: 'app-who-am-i',
+  imports: [CommonModule],
+  templateUrl: './who-am-i.component.html',
+  styleUrl: './who-am-i.component.css'
+})
+export class WhoAmIComponent implements AfterViewInit {
+  code = `
+    public class BartHermans
+    {
+        public string Name => "Bart Hermans";
+        public string Title => "Software Engineer";
+        public string[] Skills => new[] { "Java", "Angular", "C#" };
+        public string Location => "IJmuiden, Netherlands";
+
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Title} based in {Location}.");
+            Console.WriteLine("I enjoy building elegant apps and scalable systems.");
+            Console.WriteLine("Some of my key skills include:");
+            foreach (var skill in Skills)
+            {
+                Console.WriteLine($"- {skill}");
+            }
+        }
+    }
+    `;
+
+  highlightedCode: string = '';
+  languageIdentifier: string = 'csharp';
+
+  outputLines: string[] = [];
+
+  constructor(private cd: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    this.highlightedCode = Prism.highlight(this.code, Prism.languages[this.languageIdentifier], this.languageIdentifier);
+    this.cd.detectChanges();
+  }
+
+  isRunning = false;
+
+  startPrinting() {
+    if (this.isRunning) return;
+
+    this.isRunning = true;
+    this.outputLines = [];
+
+    const lines = [
+      "Hi, I'm Bart Hermans, a Software Engineer based in IJmuiden, Netherlands.",
+      "I enjoy building elegant apps and scalable systems.",
+      "Some of my key skills include:",
+      "- Java",
+      "- Angular",
+      "- C#"
+    ];
+
+    let index = 0;
+
+    const printNextLine = () => {
+      if (index < lines.length) {
+        this.outputLines.push(lines[index]);
+        index++;
+
+        const delay = 100;
+        setTimeout(printNextLine, delay);
+      } else {
+        this.isRunning = false;
+      }
+    };
+
+    printNextLine();
+  }
+}
