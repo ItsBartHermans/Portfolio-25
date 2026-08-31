407cb29c57c2adb03e36406a59f7a10087967937
Bart Hermans <bart.hermans04@gmail.com>
2026-08-31 09:01:06 +0200

    Refactor footer component and update projects list with new project details

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { Project, ProjectRowComponent } from './project-row/project-row.component';
+import {
+  Project,
+  ProjectRowComponent,
+} from './project-row/project-row.component';
 import { CommonModule } from '@angular/common';
 import { AnimateOnScrollDirective } from '../../directives/animate-on-scroll/animate-on-scroll.directive';
 
@@ -11,17 +14,22 @@ import { AnimateOnScrollDirective } from '../../directives/animate-on-scroll/ani
 export class ProjectsComponent {
   projects: Project[] = [
     {
+      title: 'Nostr-AI-damus',
+      description: `This was my project for my graduation assignment at the Hogeschool van Amsterdam. I did the project in collaboration with the company Spicelab.
+       The goal was to create a prediction model that could predict when customers would need certain products based on their previous purchases.
+       I used a combination of machine learning techniques and data analysis to build the model, and I was able to achieve a high level of accuracy in my predictions.
+       The project was a great learning experience for me, and I am proud of the work I accomplished.`,
+      urls: [],
+      imageUrl: 'assets/images/ml.png',
+    },
+    {
       title: '.GIFSplash',
       description: `Developed as a school project during my third year at HvA, this app was built by a team of four. 
       It was a great experience in collaboration and teamwork. The most fun I had was with designing and implementing 
       the websocket system. The project also challenged me with several interesting coding problems and helped me grow 
       as a developer.`,
-      urls: [
-        { url: 'https://github.com/ItsBartHermans/gifsplash-frontend', label: 'View frontend on GitHub' },
-        { url: 'https://github.com/ItsBartHermans/gifsplash-backend', label: 'View backend on GitHub' },
-      ],
-
-      imageUrl: 'assets/images/gifsplash.png'
+      urls: [],
+      imageUrl: 'assets/images/gifsplash.png',
     },
     {
       title: 'Portfolio Website',
@@ -29,9 +37,12 @@ export class ProjectsComponent {
       Building it was a fun experience, allowing me to showcase my skills and projects. 
       It also helped me improve my front-end development skills and explore new design techniques.`,
       urls: [
-        { url: 'https://github.com/ItsBartHermans/Portfolio-25', label: 'View on GitHub' }
+        {
+          url: 'https://github.com/ItsBartHermans/Portfolio-25',
+          label: 'View on GitHub',
+        },
       ],
-      imageUrl: 'assets/images/portfolio.jpg'
+      imageUrl: 'assets/images/portfolio.jpg',
     },
     {
       title: 'Atlantis 7',
@@ -39,7 +50,7 @@ export class ProjectsComponent {
       The platform supports a wide range of collections—from archives, museums, and image collections to handwritten and printed sources, art, literature, building files, deeds, and registers. 
       Working on Atlantis 7 gave me valuable experience in debugging and tracing issues within large codebases, as well as designing new systems.`,
       urls: [],
-      imageUrl: 'assets/images/naturalis.jpg'
-    }
+      imageUrl: 'assets/images/naturalis.jpg',
+    },
   ];
 }
